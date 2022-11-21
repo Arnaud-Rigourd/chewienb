394df2211bf236f61e077796085777a9f8119a02
Mathieu Warocquier <mathieu.warocquier@gmail.com>
2022-11-21 17:12:14 +0100

    modification des models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_21_154326) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_21_161024) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,7 +29,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_21_154326) do
     t.string "title"
     t.text "description"
     t.integer "rating"
-    t.text "comment"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -40,6 +39,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_21_154326) do
     t.bigint "booking_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "content"
     t.index ["booking_id"], name: "index_reviews_on_booking_id"
   end
 
