1fd4ae7a0fd43d9c0249fe8f686d9e57f4850f31
Dimitri Depardieu <dimitridepardieu@gmail.com>
2022-11-22 16:00:46 +0100

    filled bookings_policy.rb

--- app/policies/bookings_policy.rb
+++ app/policies/bookings_policy.rb
@@ -1,8 +1,24 @@
 class BookingsPolicy < ApplicationPolicy
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
-    # def resolve
-    #   scope.all
-    # end
+    def resolve
+      scope.all
+    end
+  end
+
+  def create?
+    record.user == user
+  end
+
+  def show?
+    record.user == user
+  end
+
+  def update?
+    record.user == user
+  end
+
+  def destroy
+    record.user == user
   end
 end
