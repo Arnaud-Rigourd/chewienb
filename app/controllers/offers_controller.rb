e277fa1728589d2646237e7a784e17eab4ba9d1c
Mathieu Warocquier <mathieu.warocquier@gmail.com>
2022-11-22 14:11:35 +0100

    card

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,3 +1,37 @@
 class OffersController < ApplicationController
+  def index
+    @offers = policy_scope(Offer)
+  end
 
+  def show
+    @offer = Offer.find(params[:id])
+    authorize @offer
+  end
+
+  def new
+    @offer = Offer.new
+    authorize @offer
+  end
+
+  def create
+    @offer = Offer.new(offer_params)
+    authorize @offer
+    if @offer.save
+      redirect_to offer_path(@offer)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @offer = Offer.find(params[:id])
+    @offer.destroy
+    redirect_to offers_path
+  end
+
+  private
+
+  def offer_params
+    params.require(:offer).permit(:title, :description)
+  end
 end
