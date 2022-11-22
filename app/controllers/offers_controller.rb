5a95b6cd83b5f84e70daceb862eee30cfe42cc13
arnaud rigourd <arnaud.rigourd@hotmail.fr>
2022-11-22 11:53:02 +0100

    modified review controllers and user controller

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -2,6 +2,19 @@ class OffersController < ApplicationController
   def index
   end
 
+  def new
+    @offer = Offer.new
+    authorize @offer
+  end
+
+  def create
+    @offer = Offer.new
+    authorize @offer
+    @offer.save
+  end
+
   def show
+    @offer = Offer.find(params[:id])
+    authorize @offer
   end
 end
