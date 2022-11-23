d4222587ace8c354e7f9a74228a27da09758416b
Mathieu Warocquier <mathieu.warocquier@gmail.com>
2022-11-23 22:48:01 +0100

    data controller sur le caroussel

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,7 +1,8 @@
 import { Controller } from "@hotwired/stimulus"
+import { Carousel } from "bootstrap/js/dist/carousel"
 
 export default class extends Controller {
   connect() {
-    this.element.textContent = "Hello World!"
+    new Carousel(this.element)
   }
 }
