f781a778e12138ff8c437636c80abce2de803e59
arnaud rigourd <arnaud.rigourd@hotmail.fr>
2022-11-24 17:12:36 +0100

    factorisation sur le dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,5 +11,7 @@ class PagesController < ApplicationController
     @bookings_current_user = @bookings.where(user: current_user)
     @offers = Offer.all
     @offers_current_user = @offers.where(user: current_user)
+    @bookinks_waiting_for_validation = @bookings.select { |b| b.offer.user == current_user }
+    @bookinks_waiting_for_validation_sorted = @bookinks_waiting_for_validation.sort_by{ |b| b.created_at }.reverse!
   end
 end
